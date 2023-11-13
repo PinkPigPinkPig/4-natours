1630667c665b7edf50118540cfc29c9d4e3f2ae6
GmoReview <vuquytuan2706@gmail.com>
2023-11-13 17:53:37 +0700

    create user model, signup api, encrypt password

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,51 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+// name, email, photo, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+    default: '',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password must be at least 8 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function(el) {
+        return (el === this.password);
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  console.log(this.password, this.passwordConfirm)
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
+
+const { signUp } = authController;
 
 const { getAllUSers, createUser, getUser, updateUser, deleteUser } =
   userController;
 
 const router = express.Router();
+
+router.post('/signup', signUp);
 
 router.route('/').get(getAllUSers).post(createUser);
 
