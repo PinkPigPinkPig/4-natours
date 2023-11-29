3bc460192757f9c18c4e4879ede9e55cc2ce75e9
GmoReview <vuquytuan2706@gmail.com>
2023-11-29 10:12:14 +0700

    add cors

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cors = require('cors');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // Middleware
+app.use(cors());
+
 // Security HTTP headers
 app.use(helmet());
 
